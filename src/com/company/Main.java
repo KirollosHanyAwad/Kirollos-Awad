aa141d2ca4f7c924945092917fb02e737dd8ff22
Your Name <you@example.com>
2022-02-27 16:28:45 +0100

    gh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hey brother!");
+        System.out.println("There's an endless road to rediscover");
         System.out.println("hey sister");
-        System.out.println("do u know that blood is thicker");
-        System.out.println("do u know that blood is thicker");
+        System.out.println("Know that water's sweet but blood is thicker");
     }
 }
